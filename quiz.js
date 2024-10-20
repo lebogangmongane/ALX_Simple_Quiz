a26aec3c75a6ff606b9cc8df8591ff5009b7cf4a
lebogangmongane <jerrymongane@gmail.com>
2024-10-20 20:37:06 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -16,7 +16,7 @@ function checkAnswer() {
     }
 
     // Compare the selected answer with the correct one
-    if (userAnswer.value === correctAnswer) {
+    if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done.";
         feedback.style.color = "green";
     } else {
@@ -24,6 +24,7 @@ function checkAnswer() {
         feedback.style.color = "red";
     }
 }
+
 
 // Add event listener to the submit button
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
